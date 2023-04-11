bcdce0182378a48350ff8a7c5ad5d9eea6b80dab
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-11 19:40:16 -0400

    Add spinner1.js > Emulates loading spinner within console

---
+++ spinner1.js
@@ -0,0 +1,38 @@
+//process.stdout.write('hello from spinner1.js... \rheyy\n');
+
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   '); 
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   '); 
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 1700);
